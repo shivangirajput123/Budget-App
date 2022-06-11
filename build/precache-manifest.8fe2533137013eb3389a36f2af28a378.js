eb4dde6bb7d6463a91cb4a9371072b592e09c09d
shivangirajput123 <rajputshivangiii987@gmail.com>
2022-06-12 00:26:45 +0530

    added

---
+++ build/precache-manifest.8fe2533137013eb3389a36f2af28a378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7e15582e2f4ecef477ed42d3e413da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/static/js/2.360a4eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.360a4eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b030200bb1360251c68",
+    "url": "/static/js/main.cbefc27e.chunk.js"
+  },
+  {
+    "revision": "4d75d2af8234f3d20308",
+    "url": "/static/js/runtime-main.38cb2ff9.js"
+  }
+]);
